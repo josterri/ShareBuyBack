ab1ad77292ac320ba442a845b46e4902f7fb15b3
josterri <josterri@googlemail.com>
2025-07-01 13:40:28 +0200

    Force-sync local pretradeTool to remote

--- running_benchmarks_tab.py
+++ running_benchmarks_tab.py
@@ -11,6 +11,7 @@ def run_running_benchmarks_tab(df: pd.DataFrame):
       - Cumulative (running) VWAP
       - Cumulative (running) TWAP (simple average)
       - Secondary axis: Difference between VWAP & TWAP in basis points
+      - Daily Volume (shares) & Daily Traded Value (USD)
     """
     # Prepare data
     df2 = df.copy()
@@ -21,56 +22,56 @@ def run_running_benchmarks_tab(df: pd.DataFrame):
     df2['Running VWAP'] = (df2['Close'] * df2['Volume']).cumsum() / df2['Volume'].cumsum()
     df2['Running TWAP'] = df2['Close'].expanding().mean()
     df2['Daily Price'] = df2['Close']
-
-    # Compute difference in basis points
-    # bps = (VWAP - TWAP) / TWAP * 10,000
     df2['Diff (bps)'] = (df2['Running VWAP'] - df2['Running TWAP']) \
                         / df2['Running TWAP'] * 10_000
 
-    # Build figure with secondary y-axis
-    fig = make_subplots(specs=[[{"secondary_y": True}]])
-
-    # Primary series: prices and benchmarks
-    fig.add_trace(
-        go.Scatter(
-            x=df2['Date'], y=df2['Daily Price'],
-            name='Daily Price', line=dict(color='black')
-        ),
+    # 1) Price & running benchmarks
+    fig1 = make_subplots(specs=[[{"secondary_y": True}]])
+    fig1.add_trace(
+        go.Scatter(x=df2['Date'], y=df2['Daily Price'], name='Daily Price', line=dict(color='black')),
         secondary_y=False
     )
-    fig.add_trace(
-        go.Scatter(
-            x=df2['Date'], y=df2['Running VWAP'],
-            name='Running VWAP', line=dict(color='blue')
-        ),
+    fig1.add_trace(
+        go.Scatter(x=df2['Date'], y=df2['Running VWAP'], name='Running VWAP', line=dict(color='blue')),
         secondary_y=False
     )
-    fig.add_trace(
-        go.Scatter(
-            x=df2['Date'], y=df2['Running TWAP'],
-            name='Running TWAP', line=dict(color='orange')
-        ),
+    fig1.add_trace(
+        go.Scatter(x=df2['Date'], y=df2['Running TWAP'], name='Running TWAP', line=dict(color='orange')),
         secondary_y=False
     )
-
-    # Secondary series: difference in bps
-    fig.add_trace(
-        go.Scatter(
-            x=df2['Date'], y=df2['Diff (bps)'],
-            name='VWAP − TWAP (bps)', line=dict(color='green', dash='dash')
-        ),
+    fig1.add_trace(
+        go.Scatter(x=df2['Date'], y=df2['Diff (bps)'], name='VWAP−TWAP (bps)',
+                   line=dict(color='green', dash='dash')),
         secondary_y=True
     )
-
-    # Layout
-    fig.update_layout(
-        title='Daily Price with Running VWAP, TWAP, and VWAP−TWAP (bps)',
-        xaxis_title='Date',
-        legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1)
+    fig1.update_layout(
+        title='Price & Running VWAP/TWAP (with Diff in bps)',
+        legend=dict(orientation='h', y=1.02, x=1, xanchor='right')
     )
-    fig.update_yaxes(title_text='Price', secondary_y=False)
-    fig.update_yaxes(title_text='Difference (bps)', secondary_y=True)
+    fig1.update_yaxes(title_text='Price', secondary_y=False)
+    fig1.update_yaxes(title_text='Difference (bps)', secondary_y=True)
 
-    # Render
     st.subheader("📊 Running Benchmarks")
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig1, use_container_width=True)
+
+    # 2) Daily Volume and Traded Value
+    df2['Daily Value'] = df2['Close'] * df2['Volume']
+    fig2 = make_subplots(specs=[[{"secondary_y": True}]])
+    fig2.add_trace(
+        go.Bar(x=df2['Date'], y=df2['Volume'], name='Daily Volume (shares)', marker_color='skyblue'),
+        secondary_y=False
+    )
+    fig2.add_trace(
+        go.Bar(x=df2['Date'], y=df2['Daily Value'], name='Daily Value (USD)', marker_color='lightgreen'),
+        secondary_y=True
+    )
+    fig2.update_layout(
+        title='Daily Traded Volume & Value',
+        barmode='group',
+        legend=dict(orientation='h', y=1.02, x=1, xanchor='right')
+    )
+    fig2.update_yaxes(title_text='Volume (shares)', secondary_y=False)
+    fig2.update_yaxes(title_text='Value (USD)', secondary_y=True)
+
+    st.subheader("📊 Daily Volume & Traded Value")
+    st.plotly_chart(fig2, use_container_width=True)
